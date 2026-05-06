0c14275636ae0bd418f02974877110423830d91e
Boskiomoh <boskiomoh@users.noreply.github.com>
2026-05-06 06:52:49 +0100

    feat: add outputFileTracingRoot to next config for improved build tracing

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.1.110', 'localhost:3000'],
+  outputFileTracingRoot: require('path').join(__dirname, './'),
 };
 
 export default nextConfig;
